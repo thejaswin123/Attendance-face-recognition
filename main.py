ba6968b347987dc9bc0064c827446aa340834dc1
thejaswin123 <thejas2002@yahoo.in>
2021-10-10 12:38:39 +0530

    added all files

---
+++ main.py
@@ -0,0 +1,91 @@
+import os  # accessing the os functions
+import check_camera
+import Capture_Image
+import Train_Image
+import Recognize
+
+
+# creating the title bar function
+
+def title_bar():
+    os.system('cls')  # for windows
+
+    # title of the program
+
+    print("\t**********************************************")
+    print("\t*****Attendance System Using Face Recognition*****")
+    print("\t**********************************************")
+
+
+# creating the user main menu function
+
+def mainMenu():
+    title_bar()
+    print()
+    print(10 * "*", "MENU OPTIONS", 10 * "*")
+    print("[1] Check Camera")
+    print("[2] Capture Faces")
+    print("[3] Train Images")
+    print("[4] Recognize & Attendance")
+    print("[5] Quit")
+
+    while True:
+        try:
+            choice = int(input("Enter Choice: "))
+
+            if choice == 1:
+                checkCamera()
+                break
+            elif choice == 2:
+                CaptureFaces()
+                break
+            elif choice == 3:
+                Trainimages()
+                break
+            elif choice == 4:
+                RecognizeFaces()
+                break
+            elif choice == 5:
+                print("Thank You")
+                break
+            else:
+                print("Invalid Choice. Enter 1-4")
+                mainMenu()
+        except ValueError:
+            print("Invalid Choice. Enter 1-4\n Try Again")
+    exit
+
+
+# calling the camera test function from check camera.py file
+
+def checkCamera():
+    check_camera.camer()
+    key = input("press enter to continue")
+    mainMenu()
+
+
+# calling the take image function form capture image.py file
+
+def CaptureFaces():
+    Capture_Image.takeImages()
+    key = input("press enter to continue")
+    mainMenu()
+
+
+# calling the train images from train_images.py file
+
+def Trainimages():
+    Train_Image.TrainImages()
+    key = input("press enter to continue")
+    mainMenu()
+
+
+# calling the recognize_attendance from recognize.py file
+
+def RecognizeFaces():
+    Recognize.recognize_attendence()
+    key = input("press enter to continue")
+    mainMenu()
+
+
+mainMenu()
